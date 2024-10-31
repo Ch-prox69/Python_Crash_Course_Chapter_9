8ed69231bab7d3de921a0838005eee40594ecb02
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-10-31 12:04:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+
+from restaurant import Restaurant
+
+# Create an instance of Restaurant
+my_restaurant = Restaurant("Al Dente", "Italian")
+
+# Call a method from the Restaurant class
+my_restaurant.describe_restaurant()
